010c7e63f05d65d7b46c0b040f61ebcfdd6b8e1f
overdeadright <125581361+overdeadright@users.noreply.github.com>
2025-10-27 18:23:04 +0700

    Home redirect to builder page

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/builder'
+    },
+    {
       path: '/builder',
       name: 'builder',
       component: () => import('../views/BuilderView.vue'),
